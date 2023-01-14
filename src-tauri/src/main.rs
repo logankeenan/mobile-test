2e6f7baea62f4ccde5d37ec56c2938c3740464db
Logan Keenan <d.logankeenan@gmail.com>
2023-01-14 14:50:51 -0600

    get mobile integration working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use mobile_test::AppBuilder;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,8 +12,5 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    AppBuilder::new().run();
 }
